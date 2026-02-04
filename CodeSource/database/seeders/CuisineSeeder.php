337bd758bc8d6754a4997222275692d63c009c6d
AZIZABADA10 <abadaaziz288@gmail.com>
2026-02-04 09:47:03 +0000

    feat(seeder): ajouter CuisineSeeder

---
+++ CodeSource/database/seeders/CuisineSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Cuisine;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CuisineSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Cuisine::insert([
+            ['type' => 'Marocaine'],
+            ['type' => 'Italienne'],
+            ['type' => 'Française'],
+        ]);
+    }
+}
