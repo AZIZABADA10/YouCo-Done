95a5fb6db5847ab2f1d33440b99bf0259dea8ad5
AZIZABADA10 <abadaaziz288@gmail.com>
2026-02-05 22:36:24 +0000

    feat(routes): mise à jour des routes web

--- CodeSource/routes/web.php
+++ CodeSource/routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
+use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [TestController::class,'index']);
 
 Route::middleware([
     'auth:sanctum',
@@ -15,3 +14,10 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
